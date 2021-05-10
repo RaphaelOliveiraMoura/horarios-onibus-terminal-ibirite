b77bc6e470f3586ce54456abb0ad3b015e0a5044
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2021-05-10 11:18:11 -0300

    feat: add sitemap to https://terminal-ibirite.com

---
+++ packages/web/src/pages/sitemap.xml.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import { NextPageContext } from 'next'
+
+import database from 'infra/memory-database'
+
+const BASE_URL = 'https://www.terminal-ibirite.com'
+
+class Sitemap extends React.Component {
+  static async getInitialProps({ res }: NextPageContext): Promise<void> {
+    function createSitemap() {
+      const pages: string[] = [
+        '/',
+        ...database.map((bus) => `/linhas/${bus.name}`)
+      ]
+
+      function appendPage(page: string) {
+        return ` 
+          <url>
+            <loc>${`${BASE_URL}${page}`}</loc>
+            <changefreq>hourly</changefreq>
+          </url>
+          `
+      }
+
+      return `
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${pages.map(appendPage).join('')}
+        </urlset>
+      `
+    }
+
+    res?.setHeader('Content-Type', 'text/xml')
+    res?.write(createSitemap())
+    res?.end()
+  }
+}
+
+export default Sitemap
