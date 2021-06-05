c44cdbb97832571203729c4dc431e9677ba79622
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2021-06-05 08:22:16 -0300

    feat: remove alert banner

--- packages/web/src/presentation/screens/bus-schedule-details/index.tsx
+++ packages/web/src/presentation/screens/bus-schedule-details/index.tsx
@@ -87,8 +87,6 @@ export const BusScheduleDetailsPage: React.FC<BusScheduleDetailsPageProps> = ({
 
         {!loading && (
           <>
-            <S.AlertBanner text="Sexta-feira dia 4 de junho de 2021 todos os ônibus municipais irão atuar com o horário de sábado" />
-
             <S.BusLineTitle>
               Horários da linha: <strong>{busSchedule.bus.name}</strong>
             </S.BusLineTitle>
