61cdc20df6e29fa827fd7b6819161dc9abeae128
RaphaelOliveiraMoura <raphael.oliveira@natahouse.com>
2022-02-28 14:01:56 -0300

    chore: add link & scripts tags at _document

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -4,8 +4,6 @@ import Head from 'next/head'
 import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
-  const disableAnalytics = process.env.NEXT_PUBLIC_DISABLE_ANALYTICS === 'true'
-
   return (
     <>
       <Head>
@@ -18,38 +16,6 @@ function App({ Component, pageProps }: AppProps) {
           name="description"
           content="Encontre todos horários de ônibus do terminal de ibirité sempre atualizados"
         />
-
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;400;500&display=swap"
-          rel="stylesheet"
-        />
-        {/* Google AdSense */}
-        <script
-          data-ad-client="ca-pub-3607857001646781"
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        ></script>
-        {/* Google Analytics */}
-        {process.browser && !disableAnalytics && (
-          <>
-            <script
-              async
-              src="https://www.googletagmanager.com/gtag/js?id=G-1BWK900E42"
-            />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                
-                  gtag('config', 'G-1BWK900E42');
-                 `
-              }}
-            />
-          </>
-        )}
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -8,6 +8,8 @@ import Document, {
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
+  disableAnalytics = process.env.NEXT_PUBLIC_DISABLE_ANALYTICS === 'true'
+
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
@@ -37,7 +39,39 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;400;500&display=swap"
+            rel="stylesheet"
+          />
+          {/* Google AdSense */}
+          <script
+            data-ad-client="ca-pub-3607857001646781"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
+          {/* Google Analytics */}
+          {process.browser && !this.disableAnalytics && (
+            <>
+              <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=G-1BWK900E42"
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', 'G-1BWK900E42');
+                 `
+                }}
+              />
+            </>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
