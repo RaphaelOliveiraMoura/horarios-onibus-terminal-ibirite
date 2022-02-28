b0d6a4b2805da145f5deb106c12fbd729721f820
RaphaelOliveiraMoura <raphael.oliveira@natahouse.com>
2022-02-28 18:08:55 -0300

    chore: upgrade nextjs to v12

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -3,6 +3,9 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
+  compiler: {
+    styledComponents: true
+  },
   pwa: {
     dest: 'public',
     disable: !isProd
