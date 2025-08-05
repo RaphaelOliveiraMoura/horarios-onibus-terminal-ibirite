50cae63c4523f5e95ecbfb5b6a9677b57a94e29a
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2025-08-05 19:08:10 -0300

    chore: remove storybook

--- packages/web/.storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ['../src/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials'],
-  webpackFinal: (config) => {
-    config.resolve.modules.push(`${process.cwd()}/src`)
-    return config
-  }
-}
