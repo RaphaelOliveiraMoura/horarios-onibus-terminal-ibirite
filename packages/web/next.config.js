407cc9f66c323e17b9b18cc9c09b5fb57ac48aca
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2026-05-16 17:11:55 -0300

    fix: build

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,13 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV !== 'production'
+})
 
 module.exports = withPWA({
   compiler: {
     styledComponents: true
-  },
-  pwa: {
-    dest: 'public',
-    disable: !isProd
   }
 })
